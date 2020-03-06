b07cd07b7848305ba5b4e460cb123a6bc7aefbf6
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-06 17:54:21 +0900

    開発開始

---
+++ textCamera/textCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  textCamera
+//
+//  Created by 山根大生 on 2020/03/06.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
