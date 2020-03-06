b257db6143c50e92e6bb97f64b7d69070ad937ec
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-06 18:14:08 +0900

    編集画面のview controllerとswift画面を追加

---
+++ textCamera/textCamera/EditViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EditViewController.swift
+//  textCamera
+//
+//  Created by 山根大生 on 2020/03/06.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditViewController: UIViewController{
+    
+}
