6de7d210a477d848adcb824c7823e679f99bc02f
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-06 18:04:06 +0900

    写真フォルダにアクセスできるようにした

--- textCamera/textCamera/ViewController.swift
+++ textCamera/textCamera/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goLibrary(_ sender: Any) {
+        let ipc = UIImagePickerController()
+        ipc.delegate = self
+        ipc.sourceType = UIImagePickerController.SourceType.photoLibrary
+        //編集を可能にする
+        ipc.allowsEditing = true
+        self.present(ipc,animated: true, completion: nil)
+    }
+    
 }
 
