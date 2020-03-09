737dc66953324171043e095ecfce6048e08127d2
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-09 17:14:37 +0900

    キーボードをreturnkeyでとじれるようにした

--- textCamera/textCamera/EditViewController.swift
+++ textCamera/textCamera/EditViewController.swift
@@ -9,17 +9,26 @@
 import Foundation
 import UIKit
 
-class EditViewController: UIViewController{
+class EditViewController: UIViewController, UITextFieldDelegate{
     
     
     @IBOutlet weak var getPicture: UIImageView!
     @IBOutlet weak var textField: UITextField!
     // 画像を受け取る変数
     var image: UIImage?
+    var text = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         getPicture.image = image
+        textField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+
+        textField.resignFirstResponder()
+        
+        return true
     }
 }
