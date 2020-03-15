036d0430f2adba8187b2fc2e1496178c1f619186
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-15 22:27:32 +0900

    contextのサイズを調整

--- textCamera/textCamera/EditViewController.swift
+++ textCamera/textCamera/EditViewController.swift
@@ -12,12 +12,12 @@ import UIKit
 extension UIImage{
     //写真とテロップを合成する
     func composite(image :UIImage) -> UIImage?{
-        let size = CGSize(width: self.size.width, height: self.size.height + 30)
+        let size = CGSize(width: self.size.width, height: self.size.height + self.size.height/10)
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
         let context = UIGraphicsGetCurrentContext()
         context!.setFillColor(UIColor.red.cgColor)
         context!.fill(CGRect(origin: CGPoint.zero, size: size))
-        self.draw(in: CGRect(x: 0, y: 30, width: self.size.width, height:self.size.height ))
+        self.draw(in: CGRect(x: 0, y: self.size.height/10, width: self.size.width, height:self.size.height ))
         guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
         return image
     }
