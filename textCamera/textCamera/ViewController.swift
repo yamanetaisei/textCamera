48837e11cd61835984efe7f9ec99e270606a70c6
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-04-01 15:59:18 +0900

    最初の画面の背景を変えてみた

--- textCamera/textCamera/ViewController.swift
+++ textCamera/textCamera/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let topColor = UIColor(red:0.07, green:0.13, blue:0.26, alpha:1)
+        let bottomColor = UIColor(red:0.54, green:0.74, blue:0.74, alpha:1)
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
     @IBAction func goLibrary(_ sender: Any) {
