9ba80b07d8d79be38ca757ece5d4dfd1143a8d69
yamanetaisei <swiftmantaisei@gmail.com>
2021-01-24 16:22:54 +0900

    MakeTextImage.swiftを作成、他、細かい調整

--- textCamera/textCamera/EditViewController.swift
+++ textCamera/textCamera/EditViewController.swift
@@ -14,7 +14,12 @@ class EditViewController: UIViewController, UITextFieldDelegate{
     
     
     @IBOutlet weak var getPicture: UIImageView!
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField: UITextField! {
+        didSet{
+            textField.placeholder = "表示したい文字を入力してください"
+            textField.delegate = self
+        }
+    }
     // 画像を受け取る変数
     var receivedImage: UIImage?
     var useText: String!
@@ -22,19 +27,11 @@ class EditViewController: UIViewController, UITextFieldDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let topColor = UIColor(hex: "#64b3f4")
-               let bottomColor = UIColor(hex: "#c2e59c")
-               let gradientColors: [CGColor] = [topColor!.cgColor, bottomColor!.cgColor]
-               let gradientLayer: CAGradientLayer = CAGradientLayer()
-               gradientLayer.colors = gradientColors
-               gradientLayer.frame = self.view.bounds
-               self.view.layer.insertSublayer(gradientLayer, at: 0)
-        // Do any additional setup after loading the view.
-        textField.placeholder = "表示したい文字を入力してください"
+        
         baseImage = composite(image: receivedImage!)
         getPicture.image = baseImage
-        // delegateを設定
-        textField.delegate = self
+        
+        
     }
     // キーボードのreturnが押された際に呼ばれる
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{
---
+++ textCamera/textCamera/MakeTextImage.swift
@@ -0,0 +1,9 @@
+//
+//  MakeTextImage.swift
+//  textCamera
+//
+//  Created by 山根大生 on 2021/01/24.
+//  Copyright © 2021 taisei. All rights reserved.
+//
+
+import Foundation
